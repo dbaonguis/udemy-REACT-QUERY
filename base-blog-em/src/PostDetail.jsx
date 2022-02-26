60fe80915a444a62788684303b56e23f59ca3578
Donato Baonguis <donato.baonguis@wakefern.com>
2022-02-26 12:59:40 -0500

    completed section-02---lesson-15

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -10,7 +10,7 @@ async function fetchComments(postId) {
 
 async function deletePost(postId) {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    `https://jsonplaceholder.typicode.com/posts/${postId}`,
     { method: "DELETE" }
   );
   return response.json();
@@ -27,6 +27,10 @@ async function updatePost(postId) {
 export function PostDetail({ post }) {
   const {isLoading, isError, error, data } = useQuery(['comments', post.id], () => fetchComments(post.id));
 
+  const deleteMutation = useMutation((postId) => {  // the postId argument will be populated here when the 'mutate(xxx)' function is called somewhere with the actual parameter value
+    return deletePost(postId);
+  });
+
   if (isLoading) {
     return <h3>Loading...</h3>;
   }
@@ -38,7 +42,16 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button> <button>Update title</button>
+      {deleteMutation.isError && (
+        <p style={{ color: 'red' }}>Error deleting the post</p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: 'purple' }}>Deleting the post...</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: 'green' }}>Post deleted!</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
