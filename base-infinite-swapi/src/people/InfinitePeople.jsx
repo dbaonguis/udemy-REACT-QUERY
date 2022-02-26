7bcdf9d1f7d065e0bb338e36c715a427dcc13592
Donato Baonguis <donato.baonguis@wakefern.com>
2022-02-26 15:26:36 -0500

    completed section-03---lesson-19

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -2,6 +2,7 @@ import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
+
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -2,6 +2,7 @@ import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
 
 const initialUrl = "https://swapi.dev/api/species/";
+
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
