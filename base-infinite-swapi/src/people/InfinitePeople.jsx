a9f58a01483042c359def8d44682ead83aa40650
Donato Baonguis <donato.baonguis@wakefern.com>
2022-03-12 14:59:55 -0500

    completed section-03---lesson-24

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -11,23 +11,32 @@ const fetchUrl = async (url) => {
 };
 
 const InfinitePeople = () => {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery('sw-people', ({ pageParam = initialUrl }) => {
+  const { data, fetchNextPage, hasNextPage, isFetching, isLoading, isError, error } = useInfiniteQuery('sw-people', ({ pageParam = initialUrl }) => {
     return fetchUrl(pageParam);
   }, {
     getNextPageParam: (lastPage) => lastPage.next || undefined, // return 'undefined' if this function is falsey, so that the 'hasNextPage' will return undefined
   });
 
+  if (isLoading) return <div className="loading">Loading...</div>;
+  
+  if (isError) return <div>Error! {error.toString()}</div>;
+
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
-    {
-      data?.pages?.map((pageData) => {
-        return pageData?.results?.map((person, idx) => {
-          const { name, hair_color, eye_color } = person;
-          return <Person key={idx} name={name} hairColor={hair_color} eyeColor={eye_color} />
-        });
-      })
-    }
-  </InfiniteScroll>;
+  return (
+    <>
+      {isFetching && <div className="loading">Loading (fetching)...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {
+          data.pages.map((pageData) => {
+            return pageData.results.map((person, idx) => {
+              const { name, hair_color, eye_color } = person;
+              return <Person key={idx} name={name} hairColor={hair_color} eyeColor={eye_color} />
+            });
+          })
+        }
+      </InfiniteScroll>
+    </>
+  );
 }
 
 export { InfinitePeople };
